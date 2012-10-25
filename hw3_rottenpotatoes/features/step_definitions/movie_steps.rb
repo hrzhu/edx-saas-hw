82820f22cfcabb62aa586709a5686e2189fb7d91
hrzhu <a@hrzhu.me>
2012-10-25 16:33:05 +0800

    part1: create a declarative scenario step for adding movies

--- hw3_rottenpotatoes/features/step_definitions/movie_steps.rb
+++ hw3_rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if Movie.where({:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date]}).nil?
+      Movie.create!(movie)
+    end
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
