2eeba84eb87f4ffa825378c1c8db42957b7a74c2
hrzhu <a@hrzhu.me>
2012-10-26 20:59:43 +0800

    part3: happy paths for sorting movies by title and by release date

--- hw3_rottenpotatoes/features/step_definitions/movie_steps.rb
+++ hw3_rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.should match /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
